ab154ef32501667fa7806e660ddf7d69ac164712
bhavBains <deepbains26@gmail.com>
2018-02-20 18:07:55 -0800

    Mongoose connection up, data is now recieved and sent

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -13,11 +14,50 @@ app.use(bodyParser.urlencoded({
 mongoose.connect('mongodb://localhost:27017/jobs');
 const db = mongoose.connection;
 
-//routes
-app.get('/', (req,res) => {
-  res.render('index');
+// Mongoose Schema
+const Schema = mongoose.Schema;
+const jobSchema = new Schema({
+	title: {type: String, required: true},
+	company: {type: String, required: true},
+	location: {type: String, required: true},
+	url: {type: String, required: true},
+	description: String,
+	jobType: [{type: String}],
+	coverLetter: Boolean,
 });
 
+//Mongoose Model
+const Job = mongoose.model('Job', jobSchema);
+
+//routes
+app.get('/', (req,res) => {
+	Job.find()
+		.then((doc) => {
+			console.log("data recieved, yay");
+			res.render('index', {jobs: doc});
+		});  
+});
+
+app.post('/addJob', (req, res) => {
+	const newJob = {
+		title: req.body.jobTitle,
+		company: req.body.company,
+		location: req.body.location,
+		url: req.body.jobUrl,
+		description: req.body.description,
+		jobType: req.body.jobType,
+		coverLetter: req.body.coverLetter,
+	};
+
+	const data = new Job(newJob);
+	data.save();
+	
+
+	res.redirect('/');
+});
+
+
+// Opening up the port to run the App
 app.listen(3000, () => {
 	console.log("server is up at PORT 3000")
 });
